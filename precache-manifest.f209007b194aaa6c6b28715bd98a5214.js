80a06f92591cf7e1ec042a196dfa84cb8a164550
jonathangustafsson95 <jonathangustafsson95@gmail.com>
2021-08-20 20:13:39 +0200

    upd

---
+++ precache-manifest.f209007b194aaa6c6b28715bd98a5214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410ef2990257560a3fb01245744d0df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd2bd2da1421abbc3fd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85f16babc2382045953e",
+    "url": "/static/js/2.66fd063c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.66fd063c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd2bd2da1421abbc3fd",
+    "url": "/static/js/main.487786d8.chunk.js"
+  },
+  {
+    "revision": "43879a7175c3e740616a",
+    "url": "/static/js/runtime-main.686b7e5d.js"
+  },
+  {
+    "revision": "6bfdaebc1393da79bd52de764f640050",
+    "url": "/static/media/CV.6bfdaebc.pdf"
+  }
+]);
